cf444e3846aa9480ba9e372c89a5422d6bb9885b
keisuke umezawa <keisuke-umezawa@users.noreply.github.com>
2018-12-12 21:32:38 +0900

    Update test_malmo.py

--- test_malmo.py
+++ test_malmo.py
@@ -3,29 +3,29 @@
 
 import os
 import marlo
-import cv2 as cv
-
 
 def make_env(env_seed=0):
     join_tokens = marlo.make(
         "MarLo-FindTheGoal-v0",
         params=dict(
             allowContinuousMovement=["move", "turn"],
+            videoResolution=[336, 336],
             kill_clients_after_num_rounds=500
         ))
     env = marlo.init(join_tokens[0])
 
     obs = env.reset()
-    # env.render(mode="rgb_array")
     action = env.action_space.sample()
     obs, r, done, info = env.step(action)
     env.seed(int(env_seed))
     return env
 
+
 env = make_env()
 obs = env.reset()
 
-for i in range(100):
+
+for i in range(10):
     action = env.action_space.sample()
     obs, r, done, info = env.step(action)
     print(r, done, info)
